7186142ae5220e42d36bfeb8a612d11f6fd6658c
Luciano Lorenti <lucianolorenti@gmail.com>
2017-09-15 13:43:42 -0300

    changed name to BibTeXFormat

---
+++ src/BibTeXFormat.jl
@@ -0,0 +1,11 @@
+module BibTeXFormat
+
+export BaseStyle,
+       format_entries
+using BibTeX
+include("utils.jl")
+include("person.jl")
+include("richtextelements.jl")
+include("style/style.jl")
+include("backends/backends.jl")
+end # module
