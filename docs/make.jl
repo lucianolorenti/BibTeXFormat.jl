0bb037a749bbeafc21e13c51e5f32c9088e34af1
Luciano Lorenti <lucianolorenti@gmail.com>
2017-09-19 19:57:04 -0300

    -__

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,8 @@
 import Documenter
 
 Documenter.deploydocs(
+    deps = nothing,
+    make = nothing,
     repo = "github.com/lucianolorenti/BibTeXFormat.jl.git",
     target = "build",
     julia ="0.6.0"
